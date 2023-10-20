52e42dd90dc8dcf002e692b1dc246e048dde97cd
darryle bacay <dmilesbacay@gmail.com>
2023-10-21 02:35:48 +0800

    make it more dynamic

--- PeopleSchool/CollegeList.java
+++ PeopleSchool/CollegeList.java
@@ -4,123 +4,88 @@ import java.util.Scanner;
 
 public class CollegeList {
     public static void main(String[] args) {
-        // Object calls
         Scanner sc = new Scanner(System.in);
         Employee employee = new Employee();
         Faculty faculty = new Faculty();
         Student student = new Student();
 
-        String border = "<--------------------------------->";
-
-        // Choice var
-        String choice;
-
-        // Storage
-        /* Employee */
-        double salary;
-        String department, name, contactNum;
-
-        /* Faculty */
-        String status, fName, fContact;
-        boolean regular;
+        String border = "<----------------------------------------------->";
+        String information = "information";
 
         do {
-            System.out.print("Press E for Employee, F for faculty, or S for Student: ");
-            choice = sc.nextLine();
+            System.out.print("Press 'E' for Employee, 'F' for faculty, or 'S' for student: ");
+            String choice = sc.nextLine();
 
-            if (choice.equalsIgnoreCase("E")) {
-
-                System.out.print("Enter your Name: ");
-                name = sc.nextLine();
-                employee.setName(name);
-
-                System.out.print("Enter your Contact number: ");
-                contactNum = sc.nextLine();
-                employee.setContactNum(contactNum);
-
-                System.out.print("Enter your Monthly salary: ");
-                salary = sc.nextDouble();
-                employee.setSalary(salary);
-
-                System.out.print("Enter your department: ");
-                department = sc.next();
-                employee.setDepartment(department);
-
-                System.out.println();
-
-                System.out.println(border);
-
-                System.out.println("Name: " + employee.getName());
-                System.out.println("Contact: " + employee.getContactNum());
-                System.out.println("Salary: " + employee.getSalary());
-                System.out.println("Department: " + employee.getDepartment().toUpperCase());
-
-            } else if (choice.equalsIgnoreCase("F")) {
-                System.out.print("Are you regular? Type 'y' if yes, otherwise type 'n' if not :");
-                status = sc.nextLine();
-                if (status.equalsIgnoreCase("y")) {
-                    regular = status == "Y" || status == "y";
-
-                    System.out.print("Enter your Name: ");
-                    fName = sc.nextLine();
-                    faculty.setName(fName);
-
-                    System.out.print("Enter your Contact Number: ");
-                    fContact = sc.nextLine();
-                    faculty.setContactNum(fContact);
-
-                    System.out.print("Enter your Salary: ");
-                    double fSalary = sc.nextDouble();
-                    faculty.setSalary(fSalary);
-
-                    System.out.print("Enter your Department: ");
-                    String fDepartment = sc.next();
-                    faculty.setDepartment(fDepartment);
-
+            switch (choice) {
+                case "E":
+                    System.out.println("Type Employee's name, contact number, salary & department: ");
+                    employee.setName(sc.nextLine());
+                    employee.setContactNum(sc.nextLine());
+                    employee.setSalary(sc.nextDouble());
+                    employee.setDepartment(sc.next());
                     System.out.println(border);
-
-                    System.out.println("Faculty Information");
-                    System.out.println("Name: " + faculty.getName());
-                    System.out.println("Contact Number: " + faculty.getContactNum());
-                    System.out.println("Salary: " + faculty.getSalary());
-                    System.out.println("Department: " + faculty.getDepartment().toUpperCase());
-
-                } else if (status.equalsIgnoreCase("N")) {
-                    System.out.println("Okay thank you.");
-                } else {
-                    System.out.println("Y and N only.");
-                }
-            } else if (choice.equalsIgnoreCase("S")) {
-
-                System.out.print("Enter your Name: ");
-                String sName = sc.nextLine();
-                student.setName(sName);
-
-                System.out.print("Enter your Contact Number: ");
-                String sContact = sc.nextLine();
-                student.setContactNum(sContact);
-
-                System.out.print("Enrolled Program: ");
-                String program = sc.next();
-                student.setProgram(program);
-
-                System.out.print("Year Level: ");
-                int yrLevel = sc.nextInt();
-                if (yrLevel > 4) {
-                    System.out.println("Invalid!");
-                } else {
-                    student.setYearLevel(yrLevel);
+                    System.out.println("Employee " + information);
                     System.out.println(border);
+                    System.out.println(employee.getName());
+                    System.out.println(employee.getContactNum());
+                    System.out.println(employee.getSalary());
+                    System.out.println(employee.getDepartment());
+                    break;
 
-                    System.out.println("Student Information");
-                    System.out.println("Name: " + student.getName());
-                    System.out.println("Contact Number: " + student.getContactNum());
-                    System.out.println("Program: " + student.getProgram());
-                    System.out.println("Year Level: " + student.getYearLevel());
-                }
+                case "F":
+                    System.out.println("Is the faculty Regular? Type 'Y' if yes, otherwise type 'N' if not: ");
+                    char status = sc.nextLine().toUpperCase().charAt(0);
+                    boolean isRegular = status == 'Y';
+                    faculty.setRegular(isRegular);
+
+                    if (status == 'Y') {
+                        System.out.println("Type Faculty member's name, contact number, salary :");
+                        faculty.setName(sc.nextLine());
+                        faculty.setContactNum(sc.nextLine());
+                        faculty.setSalary(sc.nextDouble());
+                        System.out.println(border);
+                        System.out.println("Faculty " + information);
+                        System.out.println(border);
+                        System.out.println(faculty.getName());
+                        System.out.println(faculty.getContactNum());
+                        System.out.println(faculty.getSalary());
+                        System.out.println("Status: " + (isRegular ? "Regular" : "Irregular"));
+                    }
+                    break;
+                case "S":
+
+                    System.out.println("Type Student's name, contact number:");
+                    student.setName(sc.nextLine());
+                    student.setContactNum(sc.nextLine());
+
+                    System.out.print("Enrolled Program: ");
+                    student.setProgram(sc.next());
+
+                    System.out.print("Year Level: ");
+                    int yrLvl = sc.nextInt();
+
+                    if (yrLvl > 4 || yrLvl < 1) {
+                        System.out.println("Invalid.");
+                    } else {
+                        student.setYearLevel(yrLvl);
+                        System.out.println(border);
+                        System.out.println("Student " + information);
+                        System.out.println(border);
+                        System.out.println(student.getName());
+                        System.out.println(student.getContactNum());
+                        System.out.println(student.getProgram().toUpperCase());
+
+                        String condition = student.getYearLevel() == 1 ? "First Year"
+                                : student.getYearLevel() == 2 ? "Second Year"
+                                        : student.getYearLevel() == 3 ? "Third Year"
+                                                : student.getYearLevel() == 4 ? "4th Year" : "";
+
+                        System.out.println(condition);
+
+                    }
 
             }
         } while (true);
-    }
 
+    }
 }
--- PeopleSchool/Faculty.java
+++ PeopleSchool/Faculty.java
@@ -3,6 +3,10 @@ package PeopleSchool;
 class Faculty extends Employee {
     private boolean status;
 
+    public void setRegular(boolean r) {
+        status = r;
+    }
+
     public boolean isRegular() {
         return status;
     }
